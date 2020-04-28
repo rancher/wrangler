0da9c1e4ff40a7975205d71dcb0c38e2d582535a
Erik Kristensen <erik@erikkristensen.com>
2020-04-28 10:26:46 -0600

    update lastUpdatedTime for StatusHandler only when the status has changed

--- pkg/controller-gen/generators/type_go.go
+++ pkg/controller-gen/generators/type_go.go
@@ -368,6 +368,9 @@ func (a *{{.lowerName}}StatusHandler) sync(key string, obj *{{.version}}.{{.type
 		}
 	}
 	if !equality.Semantic.DeepEqual(origStatus, &newStatus) {
+		// Since status has changed, update the lastUpdatedTime
+		a.condition.LastUpdated(&newStatus, time.Now().UTC().Format(time.RFC3339))
+
 		var newErr error
 		obj.Status = newStatus
 		obj, newErr = a.client.UpdateStatus(obj)
