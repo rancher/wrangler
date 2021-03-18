c12d227deb104ee08c7dc04cd04803241fbb9f31
Darren Shepherd <darren@rancher.com>
2021-03-18 09:26:32 -0700

    Add TriggerAllKey to relatedresource

--- pkg/relatedresource/changeset.go
+++ pkg/relatedresource/changeset.go
@@ -28,38 +28,6 @@ func FromString(key string) Key {
 	return NewKey(kv.RSplit(key, "/"))
 }
 
-// Look for owner references that match the apiVersion and kind and resolve to the namespace and
-// name of the parent. The namespaced flag is whether the apiVersion/kind referenced is expected to be namespaced
-func OwnerResolver(namespaced bool, apiVersion, kind string) Resolver {
-	return func(namespace, name string, obj runtime.Object) ([]Key, error) {
-		if obj == nil {
-			return nil, nil
-		}
-
-		meta, err := meta.Accessor(obj)
-		if err != nil {
-			// ignore err
-			return nil, nil
-		}
-
-		var result []Key
-		for _, owner := range meta.GetOwnerReferences() {
-			if owner.Kind == kind && owner.APIVersion == apiVersion {
-				ns := ""
-				if namespaced {
-					ns = meta.GetNamespace()
-				}
-				result = append(result, Key{
-					Namespace: ns,
-					Name:      owner.Name,
-				})
-			}
-		}
-
-		return result, nil
-	}
-}
-
 type ControllerWrapper interface {
 	Informer() cache.SharedIndexInformer
 	AddGenericHandler(ctx context.Context, name string, handler generic.Handler)
@@ -121,7 +89,7 @@ func watch(ctx context.Context, name string, enq Enqueuer, resolve Resolver, con
 	})
 
 	controller.AddGenericHandler(ctx, name, func(key string, obj runtime.Object) (runtime.Object, error) {
-		ns, name := kv.Split(key, "/")
+		ns, name := kv.RSplit(key, "/")
 		return obj, runResolve(ns, name, obj)
 	})
 }
