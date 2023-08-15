288095854c198f4fffb7df2b89dc0fec0299f4d8
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2023-08-16 02:28:14 +0300

    update client-go to 0.28 and update resource lease lock
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- pkg/leader/leader.go
+++ pkg/leader/leader.go
@@ -31,7 +31,7 @@ func run(ctx context.Context, namespace, name string, client kubernetes.Interfac
 		return err
 	}
 
-	rl, err := resourcelock.New(resourcelock.ConfigMapsLeasesResourceLock,
+	rl, err := resourcelock.New(resourcelock.LeasesResourceLock,
 		namespace,
 		name,
 		client.CoreV1(),
