d6f57f5eff31e2d65d2b6e6fb41db5637273bc8b
Silvio Moioli <silvio@moioli.net>
2023-08-28 11:38:01 +0200

    leaderelection: refactoring: extract timeout configuration setting into separate function
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- pkg/leader/leader.go
+++ pkg/leader/leader.go
@@ -51,9 +51,46 @@ func run(ctx context.Context, namespace, name string, client kubernetes.Interfac
 		logrus.Fatalf("error creating leader lock for %s: %v", name, err)
 	}
 
+	cbs := leaderelection.LeaderCallbacks{
+		OnStartedLeading: func(ctx context.Context) {
+			go cb(ctx)
+		},
+		OnStoppedLeading: func() {
+			select {
+			case <-ctx.Done():
+				// The context has been canceled or is otherwise complete.
+				// This is a request to terminate. Exit 0.
+				// Exiting cleanly is useful when the context is canceled
+				// so that Kubernetes doesn't record it exiting in error
+				// when the exit was requested. For example, the wrangler-cli
+				// package sets up a context that cancels when SIGTERM is
+				// sent in. If a node is shut down this is the type of signal
+				// sent. In that case you want the 0 exit code to mark it as
+				// complete so that everything comes back up correctly after
+				// a restart.
+				// The pattern found here can be found inside the kube-scheduler.
+				logrus.Info("requested to terminate, exiting")
+				os.Exit(0)
+			default:
+				logrus.Fatalf("leaderelection lost for %s", name)
+			}
+		},
+	}
+
+	config, err := computeConfig(rl, cbs)
+	if err != nil {
+		return err
+	}
+
+	leaderelection.RunOrDie(ctx, *config)
+	panic("unreachable")
+}
+
+func computeConfig(rl resourcelock.Interface, cbs leaderelection.LeaderCallbacks) (*leaderelection.LeaderElectionConfig, error) {
 	leaseDuration := defaultLeaseDuration
 	renewDeadline := defaultRenewDeadline
 	retryPeriod := defaultRetryPeriod
+	var err error
 	if d := os.Getenv("CATTLE_DEV_MODE"); d != "" {
 		leaseDuration = developmentLeaseDuration
 		renewDeadline = developmentRenewDeadline
@@ -61,53 +98,28 @@ func run(ctx context.Context, namespace, name string, client kubernetes.Interfac
 	if d := os.Getenv("CATTLE_ELECTION_LEASE_DURATION"); d != "" {
 		leaseDuration, err = time.ParseDuration(d)
 		if err != nil {
-			return fmt.Errorf("CATTLE_ELECTION_LEASE_DURATION value [%s] is not a valid duration: %w", d, err)
+			return nil, fmt.Errorf("CATTLE_ELECTION_LEASE_DURATION value [%s] is not a valid duration: %w", d, err)
 		}
 	}
 	if d := os.Getenv("CATTLE_ELECTION_RENEW_DEADLINE"); d != "" {
 		renewDeadline, err = time.ParseDuration(d)
 		if err != nil {
-			return fmt.Errorf("CATTLE_ELECTION_RENEW_DEADLINE value [%s] is not a valid duration: %w", d, err)
+			return nil, fmt.Errorf("CATTLE_ELECTION_RENEW_DEADLINE value [%s] is not a valid duration: %w", d, err)
 		}
 	}
 	if d := os.Getenv("CATTLE_ELECTION_RETRY_PERIOD"); d != "" {
 		retryPeriod, err = time.ParseDuration(d)
 		if err != nil {
-			return fmt.Errorf("CATTLE_ELECTION_RETRY_PERIOD value [%s] is not a valid duration: %w", d, err)
+			return nil, fmt.Errorf("CATTLE_ELECTION_RETRY_PERIOD value [%s] is not a valid duration: %w", d, err)
 		}
 	}
 
-	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
-		Lock:          rl,
-		LeaseDuration: leaseDuration,
-		RenewDeadline: renewDeadline,
-		RetryPeriod:   retryPeriod,
-		Callbacks: leaderelection.LeaderCallbacks{
-			OnStartedLeading: func(ctx context.Context) {
-				go cb(ctx)
-			},
-			OnStoppedLeading: func() {
-				select {
-				case <-ctx.Done():
-					// The context has been canceled or is otherwise complete.
-					// This is a request to terminate. Exit 0.
-					// Exiting cleanly is useful when the context is canceled
-					// so that Kubernetes doesn't record it exiting in error
-					// when the exit was requested. For example, the wrangler-cli
-					// package sets up a context that cancels when SIGTERM is
-					// sent in. If a node is shut down this is the type of signal
-					// sent. In that case you want the 0 exit code to mark it as
-					// complete so that everything comes back up correctly after
-					// a restart.
-					// The pattern found here can be found inside the kube-scheduler.
-					logrus.Info("requested to terminate, exiting")
-					os.Exit(0)
-				default:
-					logrus.Fatalf("leaderelection lost for %s", name)
-				}
-			},
-		},
+	return &leaderelection.LeaderElectionConfig{
+		Lock:            rl,
+		LeaseDuration:   leaseDuration,
+		RenewDeadline:   renewDeadline,
+		RetryPeriod:     retryPeriod,
+		Callbacks:       cbs,
 		ReleaseOnCancel: true,
-	})
-	panic("unreachable")
+	}, nil
 }
