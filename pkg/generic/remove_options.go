f7d73da95811c4516d8e1cb7f47e047ee1cffa82
Alejandro Ruiz <alejandro.ruiz@suse.com>
2024-09-11 09:31:27 +0200

    Add comment about multiple WithCondition

--- pkg/generic/remove_options.go
+++ pkg/generic/remove_options.go
@@ -5,7 +5,8 @@ import "k8s.io/apimachinery/pkg/runtime"
 // OnRemoveOption allows configuring OnRemove handlers
 type OnRemoveOption func(*objectLifecycleAdapter)
 
-// WithCondition will make OnRemove handlers to ignore resources not matching the provided condition
+// WithCondition will make OnRemove handlers to ignore resources not matching the provided condition.
+// Successive WithCondition options will replace previous ones; combining conditions needs to be done in the function being passed.
 func WithCondition(f func(runtime.Object) bool) OnRemoveOption {
 	return func(o *objectLifecycleAdapter) {
 		o.condition = f
